1c2a4859745e3c654cf310c6e5a1aac92c62106a
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2025-08-07 19:48:43 -0500

    cleaned up things i dont need

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 import express from "express";
 import firestoreRoute from "./routes/firestore.js";
-
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
-
 app.use("/firestore", firestoreRoute);
+
+app.get("/", (req, res) => {
+  res.redirect("/firestore");
+});
 
 app.listen(3000, () => {
   console.log("server running on port 3000");
